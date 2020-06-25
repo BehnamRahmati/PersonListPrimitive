dc28ed8cf48693032281b36903c9e1d57f83d938
behnam <behnam.main@gmail.com>
2020-06-25 22:06:13 +0430

    this is the first commit of person list react app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const PersonList = _ => {
+  return (
+    <section className="person__list">
+      <Person img={Math.floor(Math.random() * 10)} name="john" job="developer" />
+      <Person img={Math.floor(Math.random() * 10)} name="hossein" job="designer" />
+      <Person img={Math.floor(Math.random() * 10)} name="ali" job="principle"><p>this is paragraph about this person</p></Person>
+    </section>)
+}
+
+const Person = ({ img, name, job, children }) => {
+  return (
+    <div className="person__card" >
+      <img src={`https://randomuser.me/api/portraits/thumb/men/${img}.jpg`} alt="person" />
+      <h4>{name}</h4>
+      <h4>{job}</h4>
+      {children}
+    </div>
+  )
+}
+
+ReactDOM.render(<PersonList></PersonList>, document.getElementById("root"));
